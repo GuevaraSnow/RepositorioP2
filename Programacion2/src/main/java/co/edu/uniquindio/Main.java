d944c4bd3d14c8be30d68b56048325239045867d
Leandro337 <oscaragudelofranco@gmail.com>
2024-08-25 20:12:46 -0500

    Commit #2: Estructura del código con todos los atributos.

--- Programacion2/src/main/java/co/edu/uniquindio/Main.java
+++ Programacion2/src/main/java/co/edu/uniquindio/Main.java
@@ -1,7 +1,11 @@
 package co.edu.uniquindio.poo.torneodeportivo;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
     public static void main(String[] args) {
-
     }
 }
